27ec3fdb05c2b601347f4fc496233118cac6ae59
Ausername534 <bneri700@gmail.com>
2026-04-04 08:59:17 -0500

    changes
    Added comments and program description for software sales calculation.

--- software sales c++.cpp
+++ software sales c++.cpp
@@ -1,3 +1,17 @@
+//assignment:
+/*
+A software company sells a package that retails for $99. Quantity discounts are given according to the following table.
+
+Quantity	Discount
+10–19	20%
+20–49	30%
+50–99	40%
+100 or more	50%
+Write a program that asks for the number of units sold and computes the total cost of the purchase.
+
+Be efficient! Points will be deducted for a program that requires more test (e.g., if statements) than necessary to be executed.
+*/
+
 #include <iostream>
 using namespace std;
 
