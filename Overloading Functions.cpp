60a4b9cc2d8d36e2e3439f47f0297b9f1aab33ad
Ausername534 <bneri700@gmail.com>
2026-04-06 15:45:29 -0500

    Implement hospital charge calculation with overloading
    The program calculates hospital charges based on inpatient or outpatient status. It includes overloaded functions to handle different charge calculations.

---
+++ Overloading Functions.cpp
@@ -0,0 +1,93 @@
+/** assignment
+Write a program that computes and displays the charges for a patient’s hospital stay. First, the program should ask if the patient was admitted as an inpatient or an outpatient.
+
+If the patient was an inpatient, the following data should be entered:
+
+The number of days spent in the hospital
+The daily rate
+Hospital medication charges
+Charges for hospital services (lab tests, etc.)
+
+The program should ask for the following data if the patient was an outpatient:
+
+Charges for hospital services (lab tests, etc.)
+Hospital medication charges
+**/
+
+
+
+
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <string>
+
+
+// inpatient version
+double charges(int days, double dailyrate, double medchanges, double servicesCharges) {
+	return days * dailyrate + medchanges + servicesCharges;
+}
+
+// outpatient version
+double charges(double services, double medication) {
+	return services + medication;
+
+}
+
+
+int main() {
+
+	
+
+
+	char choice;
+	std::cout << " Was the patient an inpatient or outpatient? Enter 'I' for inpatient or 'O' for outpatient: ";
+	std::cin >> choice;
+
+
+	if (choice == 'I' || choice == 'i') {
+		int days;
+		double dailyrate;
+		double medchanges;
+		double serviceCharges;
+
+
+		std::cout << "Enter the Number of days spent in the hospital: ";
+		std::cin >> days;
+		std::cout << "Daily rate: ";
+		std::cin >> dailyrate;
+		std::cout << "Hospital medication charges: ";
+		std::cin >> medchanges;
+		std::cout << "Charges for hospital services (lat tests, etc.): ";
+		std::cin >> serviceCharges;
+
+
+
+		double total = charges(days, dailyrate, medchanges, serviceCharges);
+		std::cout << "\nTotal charges for inpatient: $" << total << '\n';
+	}
+	else if (choice == 'O' || choice == 'o') {
+		double medChanges;
+		double serviceCharges;
+
+
+		std::cout << "Hospital medication charges: ";
+		std::cin >> medChanges;
+		std::cout << "Charges for hospital services (lab tests, etc.): ";
+		std::cin >> serviceCharges;
+
+		double total = charges(medChanges, serviceCharges);
+		std::cout << "\nTotal charges for outpatient: $" << total << '\n';
+	}
+	else {
+		std::cout << "Invalid selection. Please run the program again and enter 'I' or 'O'.\n";
+	}
+
+
+
+
+
+	return 0;
+
+
+}
